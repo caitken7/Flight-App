388c511bb2c59abdf09df263cb66b534e091f5fe
Stephanie Wertz <stephaniewertz@Stephanies-MacBook-Air.local>
2017-10-05 11:28:32 -0500

    js added

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+ var config = {
+    apiKey: "AIzaSyBdRes2snb0kg27ahUhZSCF6t9Q2Pke_0c",
+    authDomain: "flight-app-a6688.firebaseapp.com",
+    databaseURL: "https://flight-app-a6688.firebaseio.com",
+    projectId: "flight-app-a6688",
+    storageBucket: "",
+    messagingSenderId: "861898449579"
+  };
+  firebase.initializeApp(config);
+
+  
